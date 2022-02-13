f554f49b9906b274346b8c6f2505e91a178b110c
Mehran Azam <moinulazam@MehranAcBookPro.home>
2022-02-13 16:45:04 -0500

    Work in Progress

--- week-01/assessment-1/src/main/java/HotelApp.java
+++ week-01/assessment-1/src/main/java/HotelApp.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 public class HotelApp {
 
@@ -7,12 +8,14 @@ public class HotelApp {
         System.out.println("=====================");
 
         int capsules = capsuleCount("How many capsules are available?: ");
+        mainMenu();
 
-        int menuOptions = mainMenu("Choose an option [1-4]: ");
 
 
 
     }
+
+    private static Random rng = new Random();
 
     public static int capsuleCount (String prompt){
         Scanner scn = new Scanner(System.in);
@@ -25,36 +28,123 @@ public class HotelApp {
         return userInt;
     }
 
-    public static int mainMenu(String prompt){
-        Scanner scan1 = new Scanner(System.in);
+    public static void mainMenu(){
+
         System.out.println("Guest Menu");
         System.out.println("====================");
         System.out.println("1. Check in");
         System.out.println("2. Check out");
         System.out.println("3. View Guests");
         System.out.println("4. Exit");
+        System.out.println("Choose an option [1-4]: ");
 
-        int option = scan1.nextInt();
-        return option;
+
 
     }
 
-//    public static String name(){
+    public static int getIntInRange(){
+
+        int choice = Integer.MIN_VALUE;
+        Scanner scan1 = new Scanner(System.in);
+
+        choice = scan1.nextInt();
+        switch(choice) {
+            case 1:
+                checkIn();
+                break;
+            case 2:
+                checkOut();
+                break;
+            case 3:
+                viewRooms();
+                break;
+            case 4:
+                exit();
+                break;
+            default:
+                System.out.println("Unknown action");
+                choice = scan1.nextInt();
+
+        }
+        return choice;
+
+        }
+
+
+
+
+
+
+
+    public static String name(String guestName){
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("Guest's name: ");
+        guestName = scan2.nextLine();
+
+        return guestName;
+            }
 //
+    public static int checkIn(int roomAvail){
+        Scanner scan3 = new Scanner(System.in);
+        String guestName = "";
+        System.out.println("Guest Check In");
+        System.out.println("===============");
+        System.out.println("Guest Name: " + name(guestName));
+        System.out.print("Capsule #[1-" + capsules]: + " ");
+        roomAvail = scan3.nextInt();
+
+       return roomAvail;
+    }
+
+    public static boolean isRoomAvailable(int roomAvail){
+        boolean occupied = false;
+
+        if(checkIn(roomAvail) ){
+            occupied = true;
+        }else{
+            occupied = false;
+        }
+        return occupied;
+    }
 //
-//    }
+    public static int checkOut(){
+        boolean occupied = false;
+        if(){
+
+        }
+
+    }
 //
-//    public static int checkIn(){
-//
-//    }
-//
-//    public static int checkOut(){
-//
-//
-//    }
-//
-//    public static boolean exit(){
-//
-//
-//    }
+    public static boolean exit(boolean doExit){
+        doExit = false;
+        Scanner scan4 = new Scanner(System.in);
+        System.out.println("Exit");
+        System.out.println("====");
+        System.out.println("Are you sure you want to exit?");
+        System.out.println("All data will be lost.");
+        System.out.println("Exit [y/n]: ");
+        String exit1 = scan4.nextLine();
+        if(exit1.equals("y")){
+            doExit = true;
+            System.out.println("Goodbye!");
+        }
+        else if(exit1.equals("n")){
+            doExit = false;
+            mainMenu();
+        }
+        return doExit;
+    }
+
+
+
+    public static void viewRooms(int capsules){
+        Random random = new Random();
+        capsules = capsuleCount();
+        StringBuilder occupant = new StringBuilder();
+
+        for(int i = 0; i < capsules; capsules++){
+
+        }
+        return occupant;
+    }
 }
--- week-01/tic-tac-toe/src/main/java/App.java
+++ week-01/tic-tac-toe/src/main/java/App.java
@@ -3,119 +3,151 @@ import java.util.Scanner;
 
 public class App {
 
+    public static void main( String[] args ){
 
-    public static void main(String[] args) {
-        //Goal: play through one game of tic tac toe with computer
+        //Goal: play through one game of tic-tac-toe with the computer
         //      we'll let the player always go first
 
-
-
-        // representing state of game
+        // representing the state of the game
         //      2d array of ints
         //      1:      X
         //      -1:     O
         //      0:      blank
 
-        //loop until game is over
+        //loop until the game is over
         //first let the player move,
-        //      let player enter square they want
-        //      using numberpad locations
+        //      let the player enter the square they want
+        //      using the numberpad locations
         //
-        //      7   8   9
-        //      4   5   6
-        //      1   2   3
+        //      7  8  9
+        //      4  5  6
+        //      1  2  3
 
 
-
-        //  [0][0]  [0][1]  [0][2]      first index is row
-        //  [1][0]  [1][1]  [1][2]      second index is column
-        //  [2][0]  [2][1]  [2][2]
-
-        //check for winner
-
-        //then let computer move in blank spot
+        //   [0][0]  [0][1]   [0][2]        first index is the row
+        //   [1][0]  [1][1]   [1][2]        second index is the col
+        //   [2][0]  [2][1]   [2][2]
 
         //check for winner
 
-        //initializing to a new board of all blank squares
+        //then let the computer move in a blank spot
+
+        //check for winner
+
+        //intializing to a new board of all blank squares
+        //because 0 is blank
         int[][] board = new int[3][3];
 
-        while(!isWinner(board)){
+        while(!allSquaresFilled(board) && !hasWinner( board )){
             //get move from player
 
             int playerMove = getMoveFromPlayer(board);
 
-
             //compute coordinate
-            int row = calcRow(playerMove);
-            int col = calcCol(playerMove);
-
+            int row = calcRow( playerMove );
+            int col = calcCol( playerMove );
 
             //mark x on the board
             board[row][col] = 1;
 
             //check if the game is over
-            if(!isWinner(board)){
-                //if not, get random move from computer
-                int computerMove = getMoveFromComp(board);
+            if( !allSquaresFilled(board) && !hasWinner(board)){
+                //if not, get a random move from the computer
+                int computerMove = getMoveFromComp( board );
 
-                row = calcRow(computerMove);
-                col = calcCol(computerMove);
-
+                row = calcRow( computerMove );
+                col = calcCol( computerMove );
 
                 //marking an o on the board
                 board[row][col] = -1;
             }
 
-
-
-            //if not, get random move from computer
-
-
         }
+
+        //final print after the game is over
         printBoard(board);
-
-
-
 
     }
 
     private static Random rng = new Random();
-
     private static int getMoveFromComp(int[][] board) {
         //randomly generate a number from 1-9
         //check if the square at that position is blank
-        //if not, loop back to start
+        //if not, loop back to the start
         int compMove = Integer.MIN_VALUE;
 
         boolean valid = false;
 
-        while(!valid){
+        while( !valid ){
             //rng.nextInt(x)
             //      generates a random number between 0 and x-1
             //      each random number has an equal probability
             //      of being selected
 
             compMove = 1 + rng.nextInt(9);
-            int row = calcRow(compMove);
-            int col = calcCol(compMove);
+            int row = calcRow( compMove );
+            int col = calcCol( compMove );
 
-            if(board[row][col] == 0){
+            if( board[row][col] == 0){
                 valid = true;
             }
+
         }
 
-        //TODO: actually implement this method
-
-        return -1;
+        return compMove;
     }
 
+
+    //      7  8  9
+    //      4  5  6
+    //      1  2  3
+
+    //subtracting 1
+
+    //      6  7  8
+    //      3  4  5
+    //      0  1  2
+
+
+    //   [0]  [1]   [2]
+    //   [0]  [1]   [2]
+    //   [0]  [1]   [2]
     private static int calcCol(int move) {
         return (move - 1) % 3;
     }
 
+
+    //      7  8  9
+    //      4  5  6
+    //      1  2  3
+
+    //        -1
+
+
+    //      6  7  8
+    //      3  4  5
+    //      0  1  2
+
+    //         /3
+
+    //      2  2  2
+    //      1  1  1
+    //      0  0  0
+
+    //         2-x
+
+    //      0  0  0
+    //      1  1  1
+    //      2  2  2
+
+    //   [0]  [0]   [0]
+    //   [1]  [1]   [1]
+    //   [2]  [2]   [2]
+
+
     private static int calcRow(int move) {
-        return 2 - ((move - 1) / 3);
+
+        return 2 - (move - 1)/3;
     }
 
     private static int getMoveFromPlayer(int[][] board) {
@@ -125,50 +157,57 @@ public class App {
 
         boolean valid = false;
 
-        while(!valid){
-            printBoard(board);
-            System.out.print("Please enter your move (keypad): ");
+        while( !valid ){
+
+            printBoard( board );
+
+            System.out.print( "Please enter your move (keypad): ");
+
             playerMove = scn.nextInt();
 
-            if(playerMove >= 1 && playerMove <= 9){
+            if( playerMove >= 1 && playerMove <= 9 ) {
                 int row = calcRow(playerMove);
                 int col = calcCol(playerMove);
 
-                if(board[row][col] == 0){
+                if (board[row][col] == 0) {
                     valid = true;
                 }
             }
         }
+
         return playerMove;
     }
 
-    public static boolean isWinner(int[][] boardToCheck){
+    public static boolean hasWinner( int[][] boardToCheck ){
         boolean winner = false;
 
         //checking rows
-        for(int row = 0; row < 3; row++){
+        for( int row = 0; row < 3; row++){
             int sum = 0;
 
-            for(int col = 0; col < 3; col++){
+            for( int col = 0; col < 3; col++){
+                sum += boardToCheck[row][col];
+            }
+
+            //square the sum, 9 means we got 3 or -3 which means 3 marks in a row
+            sum *= sum;
+
+            if( sum == 9 ){
+                winner = true;
+            }
+        }
+
+        //checking columns
+        for( int col = 0; col < 3; col++ ){
+            int sum = 0;
+
+            for( int row = 0; row < 3; row++){
                 sum += boardToCheck[row][col];
             }
 
             sum *= sum;
 
-            if(sum == 9){
-                winner = true;
-            }
-
-        }
-
-        for(int col = 0; col < 3; col++) {
-                int sum = 0;
-            for (int row = 0; row < 3; row++) {
-                    sum += boardToCheck[row][col];
-            }
-            sum *= sum;
-
-            if(sum == 9){
+            if( sum == 9 ){
                 winner = true;
             }
         }
@@ -179,37 +218,49 @@ public class App {
         int diag2Sum = boardToCheck[2][0] + boardToCheck[1][1] + boardToCheck[0][2];
         diag2Sum *= diag2Sum;
 
+        if( diag1Sum == 9 || diag2Sum == 9 ){
+            winner = true;
+        }
 
         return winner;
+
     }
 
-//    private static boolean allSquaresFilled(int[][] boardToCheck){
-//
-//        boolean allFilled = true;
-//
-//        for(int row = 0; row < 3; row++){
-//            for(int col = 0; col < 3; col++){
-//                if(boardToCheck){
-//
-//                }
-//            }
-//        }
-//
-//    }
+    private static boolean allSquaresFilled( int[][] boardToCheck ){
 
-    public static void printBoard(int[][] boardToPrint){
-        for(int row = 0; row < 3; row++) {
+        boolean allFilled = true;
 
-
-            for (int col = 0; col < 3; col++) {
-                if(boardToPrint[row][col] == 0){
-                    System.out.print(".");
-                }else{
-                    String toPrint = boardToPrint[row][col] == 1 ? "X" : "O";
-                    System.out.print(toPrint);
+        for( int row = 0; row < 3; row++){
+            for( int col = 0; col < 3; col++ ){
+                if( boardToCheck[row][col] == 0 ){
+                    allFilled = false;
                 }
+            }
+        }
+
+
+
+        return allFilled;
+    }
+
+    public static void printBoard( int[][] boardToPrint ){
+
+        for( int row = 0; row < 3; row++){
+            for( int col = 0; col < 3; col++ ){
+
+                if( boardToPrint[row][col] == 0){
+                    System.out.print(".");
+                } else {
+                    //ternary operator
+                    //                boolean condition     true val  false val
+
+                    String toPrint = boardToPrint[row][col] == 1 ? "X" : "O";
+                    System.out.print( toPrint );
+                }
+
             }
             System.out.println();
         }
     }
+
 }
