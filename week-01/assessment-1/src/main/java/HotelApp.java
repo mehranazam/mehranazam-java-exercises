7fcc5282438481f3ffb787062560eb7a0b9b46e8
Mehran Azam <moinulazam@MehranAcBookPro.home>
2022-02-13 23:47:10 -0500

    Work in progress

--- week-01/assessment-1/src/main/java/HotelApp.java
+++ week-01/assessment-1/src/main/java/HotelApp.java
@@ -6,30 +6,63 @@ public class HotelApp {
 
         System.out.println("Welcome to Hotel 69.");
         System.out.println("=====================");
+        int capsules = 0;
+        int roomAvail = 0;
 
-        int capsules = capsuleCount("How many capsules are available?: ");
+      //  int capsules = capsuleCount("How many capsules are available?: ");
+        String[] capsules1 = new String[capsuleCount(capsules)];
+        System.out.println("There are " + capsules + " unoccupied capsules ready to be booked.");
+
         mainMenu();
+
+        int choice = Integer.MIN_VALUE;
+        Scanner scan1 = new Scanner(System.in);
+
+        choice = scan1.nextInt();
+        do {
+            switch (choice) {
+                case 1:
+                    checkIn(roomAvail, capsules1);
+                    break;
+                case 2:
+                    checkOut();
+                    break;
+                case 3:
+                    viewRooms(capsules1);
+                    break;
+                case 4:
+                    exit();
+                    break;
+                default:
+                    System.out.println("Unknown action");
+                    choice = scan1.nextInt();
+            }while (!(choice == 4));
+
+        }
+
+
 
 
 
 
     }
 
-    private static Random rng = new Random();
+  //  private static Random rng = new Random();
 
-    public static int capsuleCount (String prompt){
-        Scanner scn = new Scanner(System.in);
-        System.out.print(prompt);
+    public static int capsuleCount (int capsules){
+        Scanner scan1 = new Scanner(System.in);
+        System.out.print("How many capsules are there?");
+        String count = scan1.nextLine();
 
         int userInt = Integer.MIN_VALUE;
 
-        userInt = Integer.parseInt(scn.nextLine());
+        userInt = Integer.parseInt(count);
 
         return userInt;
     }
 
     public static void mainMenu(){
-
+        Scanner scn = new Scanner(System.in);
         System.out.println("Guest Menu");
         System.out.println("====================");
         System.out.println("1. Check in");
@@ -40,35 +73,39 @@ public class HotelApp {
 
 
 
+
     }
 
-    public static int getIntInRange(){
-
-        int choice = Integer.MIN_VALUE;
-        Scanner scan1 = new Scanner(System.in);
-
-        choice = scan1.nextInt();
-        switch(choice) {
-            case 1:
-                checkIn();
-                break;
-            case 2:
-                checkOut();
-                break;
-            case 3:
-                viewRooms();
-                break;
-            case 4:
-                exit();
-                break;
-            default:
-                System.out.println("Unknown action");
-                choice = scan1.nextInt();
-
-        }
-        return choice;
-
-        }
+//    public static int getIntInRange(){
+//
+//        int choice = Integer.MIN_VALUE;
+//        Scanner scan1 = new Scanner(System.in);
+//
+//        choice = scan1.nextInt();
+//        do {
+//            switch (choice) {
+//                case 1:
+//                    checkIn();
+//                    break;
+//                case 2:
+//                    checkOut();
+//                    break;
+//                case 3:
+//                    viewRooms();
+//                    break;
+//                case 4:
+//                    exit();
+//                    break;
+//                default:
+//                    System.out.println("Unknown action");
+//                    choice = scan1.nextInt();
+//            }
+//            while (!(choice == 4)) ;
+//
+//        }
+//        return choice;
+//
+//        }
 
 
 
@@ -84,13 +121,13 @@ public class HotelApp {
         return guestName;
             }
 //
-    public static int checkIn(int roomAvail){
+    public static int checkIn(int roomAvail, int[] capsules1){
         Scanner scan3 = new Scanner(System.in);
         String guestName = "";
         System.out.println("Guest Check In");
         System.out.println("===============");
         System.out.println("Guest Name: " + name(guestName));
-        System.out.print("Capsule #[1-" + capsules]: + " ");
+        System.out.print("Capsule #[1-" + capsuleCount() + ")]:  " + " ");
         roomAvail = scan3.nextInt();
 
        return roomAvail;
@@ -98,6 +135,7 @@ public class HotelApp {
 
     public static boolean isRoomAvailable(int roomAvail){
         boolean occupied = false;
+        roomAvail = 0;
 
         if(checkIn(roomAvail) ){
             occupied = true;
@@ -107,8 +145,14 @@ public class HotelApp {
         return occupied;
     }
 //
-    public static int checkOut(){
+    public static int checkOut(int roomOccupied){
         boolean occupied = false;
+        Scanner scan3 = new Scanner(System.in);
+        String guestName = "";
+        System.out.println("Guest Check Out");
+        System.out.println("===============");
+        System.out.print("Capsule #[1-" + capsuleCount() + ")]:  " + " ");
+        roomOccupied = scan3.nextInt();
         if(){
 
         }
@@ -137,14 +181,27 @@ public class HotelApp {
 
 
 
-    public static void viewRooms(int capsules){
-        Random random = new Random();
-        capsules = capsuleCount();
-        StringBuilder occupant = new StringBuilder();
+    public static void viewRooms(String[] capsules1){
+        Scanner scan4 = new Scanner(System.in);
+//        Random random = new Random();
+//        capsules = capsuleCount();
+//        StringBuilder occupant = new StringBuilder();
 
-        for(int i = 0; i < capsules; capsules++){
+        String guestName = "";
+        System.out.println("View Guests");
+        System.out.println("============");
+        System.out.println("Capsule #[1-" + capsuleCount() + ")]:  " + " ");
+        int capsuleNumber = Integer.parseInt(scan4.nextLine());
+        int max = capsuleNumber + 6;
+        int min = capsuleNumber - 5;
 
+        for(int i = min; i < max; i++){
+            if(max > capsules1.length){
+                i = max % capsules1.length;
+            }
+            guestName = capsules1[i];
+            System.out.println(i + ": " + guestName);
         }
-        return occupant;
+        return;
     }
 }
