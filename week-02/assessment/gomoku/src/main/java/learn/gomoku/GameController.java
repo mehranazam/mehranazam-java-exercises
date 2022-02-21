71f7558eac6e1acfa4701cf5b23866ec274a285f
Mehran Azam <moinulazam@MehranAcBookPro.home>
2022-02-21 18:07:39 -0500

    Assessment 2

--- week-02/assessment/gomoku/src/main/java/learn/gomoku/GameController.java
+++ week-02/assessment/gomoku/src/main/java/learn/gomoku/GameController.java
@@ -15,7 +15,6 @@ import static learn.gomoku.game.Gomoku.WIDTH;
 public class GameController {
 
     private Scanner console = new Scanner(System.in);
-//    private char[][] board = new char[WIDTH][WIDTH];
     Gomoku game;
 
 
@@ -23,33 +22,27 @@ public class GameController {
         setup();
         play();
         playAgain();
-
     }
 
 
     private void setup(){
+        System.out.println("Player 1 is: ");
         Player player1 = getPlayer(1);  // gets one player
+        System.out.println("Player 2 is: ");
         Player player2 = getPlayer(2);
         game = new Gomoku(player1, player2);
-
-
-
-//        return game;
     }
 
     private Player getPlayer(int playerNumber){
-//        System.out.println("Welcome to Gomoku");
-//        System.out.println("===================");
-//        System.out.println("Player 1 is:\n1. Human\n2. Random Player\nSelect [1-2]: ");
-//        int playerNum = Integer.parseInt(console.nextLine());
-            playerNumber = readInt("Player 1 is:\n1. Human\n2. Random Player\nSelect [1-2]: ", 1, 2);
+
+            playerNumber = readInt("\n1. Human\n2. Random Player\nSelect [1-2]: ", 1, 2);
         if(playerNumber == 1) {
-            Player player = new HumanPlayer(readRequiredString("Player 1, enter your name: "));
+            Player player = new HumanPlayer(readRequiredString("Enter your name: "));
             return player;
 
         }else {
             Player computer = new RandomPlayer();
-          
+
             return computer;
         }
 
@@ -75,13 +68,7 @@ public class GameController {
         }
     }
 
-//    private Stone findStoneAt(List<Stone> toSearch, int row, int col){
-//        for(int row = 0; row < WIDTH; row++){
-//            for(int col = 0; col < WIDTH; col++) {
-//
-//            }
-//            }
-//    }
+
 
     private void printBoard(){
         List<Stone> currentStones = game.getStones();
@@ -106,64 +93,7 @@ public class GameController {
             }
             System.out.println();
         }
-
-//        if(currentStones.isEmpty()){
-//            for(int i = 0; i < board.length; i++){
-//                    if(i == 0){
-//                        System.out.println(" ");
-//                    }
-//                System.out.printf("%02d  ", (i + 1));
-//            }
-//            System.out.println();
-//            for(int row = 0; row < board.length; row++){
-//                for(int col = 0; col < board[row].length; col++){
-//                    if(col == 0){
-//                        System.out.printf("%02d", (row + 1));
-//                    }
-//                    System.out.printf("%4s", board[row][col] + " ");
-//                }
-//            }
-//        }else{
-//            for(int row = 0; row < currentStones.size(); row++){
-//                Stone stone = currentStones.get(row);
-//                for(int col = 0; col < currentStones.size(); col++){
-//                        if(stone.getRow() == (row + 1) && stone.getColumn() == (col + 1)){
-//                                board[row][col] = 'x';
-//                        }
-//                }
-//            }
-//
-//            for(int i = 0; i < board.length; i++){
-//                    if(i == 0){
-//                        System.out.println(" ");
-//                    }
-//                System.out.printf("%02d  ", (i + 1));
-//            }
-//            System.out.println();
-//            for(int j = 0; j < board.length; j++){
-//
-//            }
-//        }
-
-
-
-
-//        for(int row = 0; row < WIDTH; row++){
-//            for(int col = 0; col < WIDTH; col++){
-//                if(board[row][col] == 0){
-//                System.out.print("_");
-//            }else{
-//                    String toPrint = board[row][col] == 1 ? "X" : "O";
-//                    System.out.println(toPrint);
-//                }
-//        }
-//            System.out.println();
-//    }
-//
-
-//        game.board();
     }
-
     private String readRequiredString(String message){
         System.out.println(message);
         String input = console.nextLine();
@@ -173,9 +103,7 @@ public class GameController {
             input = console.nextLine();
         }
             return input;
-
     }
-
     private int readInt(String message, int min, int max){
         System.out.println(message);
 
@@ -186,13 +114,9 @@ public class GameController {
             playerNum = Integer.parseInt(console.nextLine());
         }
             return playerNum;
-
     }
-
     private boolean playAgain(){
         boolean choice = false;
-//        readRequiredString("Do you want to play again?");
-
         if(readRequiredString("Do you want to play again?").toLowerCase().equals("yes")){
             choice = true;
         }else{
