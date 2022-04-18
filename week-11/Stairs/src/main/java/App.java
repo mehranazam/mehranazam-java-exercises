dd81ce37a55afafbf44ebeca69a49e29d7563eaf
unknown <mazam@dev-10.com>
2022-04-18 18:01:12 -0400

    Modified

--- week-11/Stairs/src/main/java/App.java
+++ week-11/Stairs/src/main/java/App.java
@@ -4,31 +4,38 @@ import java.util.List;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(climbingStairs(4));
     }
 
-    public static int climbingStairs(int n){
+    public static int climbingStairs(int n) {
         List<Integer> toReturn = new ArrayList<>();
 
         toReturn.add(n);
+        boolean end = true;
 
 
-        for(int i = 0; i < toReturn.size(); i++){
-
+        do{
+            end = true;
             List<Integer> newArr = new ArrayList<>();
+            for (int i = 0; i < toReturn.size(); i++) {
+                if (toReturn.get(i) == 0) {
+                    newArr.add(0);
+                } else {
+                    end = false;
+                    if (toReturn.get(i) - 1 >= 0) {
+                        newArr.add(toReturn.get(i) - 1);
+                    }
+                    if (toReturn.get(i) - 2 >= 0) {
+                        newArr.add(toReturn.get(i) - 2);
+                    }
+                }
 
-            if(toReturn.get(i) == 0){
-                continue;
-            }else{
-                if(toReturn.get(i) - 1 >= 0){
-                    newArr.add(toReturn.get(i) - 1);
-                }
-                if(toReturn.get(i) - 2 >= 0){
-                    newArr.add(toReturn.get(i) - 2);
-                }
+
+
+
             }
-
             toReturn = newArr;
-        }
+        }while (!end);
+        return toReturn.size();
     }
 }
